008364f68df4cf1edef1cd7fa73e70cf8122f728
XinHallow <xinhallow@outlook.com>
2026-02-13 13:19:56 +0800

    feat: add firecracker api definition

--- lib/firecracker-sdk/src/api.rs
+++ lib/firecracker-sdk/src/api.rs
@@ -1,4 +1,5 @@
 //! Firecracker API client
+use crate::dto;
 use http_body_util::{BodyExt, Full};
 use hyper::{
     Method, Request, Response, StatusCode, Uri,
@@ -6,6 +7,7 @@ use hyper::{
 };
 use hyper_util::client::legacy::Client;
 use hyperlocal::{UnixClientExt, UnixConnector, Uri as UnixUri};
+use paste::paste;
 use serde::Deserialize;
 use std::path::PathBuf;
 
@@ -106,26 +108,178 @@ impl FirecrackerApiClient {
     async fn put<T>(
         &self,
         path: &str,
-        req: Full<Bytes>,
+        req: Vec<u8>,
         expected_status: StatusCode,
     ) -> Result<T, ApiError>
     where
         T: for<'de> serde::Deserialize<'de>,
     {
-        let result = self.request(Method::GET, path, req).await?;
+        let result = self.request(Method::GET, path, req.into()).await?;
         self.parse_response(result, expected_status).await
     }
 
     async fn patch<T>(
         &self,
         path: &str,
-        req: Full<Bytes>,
+        req: Vec<u8>,
         expected_status: StatusCode,
     ) -> Result<T, ApiError>
     where
         T: for<'de> serde::Deserialize<'de>,
     {
-        let result = self.request(Method::PATCH, path, req).await?;
+        let result = self.request(Method::PATCH, path, req.into()).await?;
         self.parse_response(result, expected_status).await
+    }
+}
+
+// HACK: Improve this shit
+macro_rules! api_methods {
+    (
+        $(
+            $method:ident $path:literal
+            as $fn_name:ident
+            $(-> $ret:tt)?
+            $(($param_name:ident: $param:tt))?
+            with $status:ident
+        );* $(;)?
+    ) => {
+        impl FirecrackerApiClient {
+            $(
+                api_methods!(@method
+                    $method $path $fn_name
+                    $($ret)?
+                    $(($param_name $param))?
+                    $status
+                );
+            )*
+        }
+    };
+
+    // GET
+    (@method GET $path:literal $fn_name:ident $ret:tt $status:ident) => {
+        pub async fn $fn_name(&self) -> Result<dto::$ret, ApiError> {
+            self.get($path, StatusCode::$status).await
+        }
+    };
+
+    // PUT/PATCH
+    (@method $method:ident $path:literal $fn_name:ident ($param_name:ident $param:tt) $status:ident) => {
+        paste! {
+            pub async fn $fn_name(
+                &self,
+                $param_name: &dto::$param,
+            ) -> Result<(), ApiError> {
+                self.[<$method:lower>](
+                    $path,
+                    serde_json::to_vec(&$param_name)?,
+                    StatusCode::$status,
+                )
+                .await
+            }
+        }
+    };
+}
+
+api_methods!(
+    GET "/" as get_instance_info -> InstanceInfo with OK;
+    PUT "/actions" as put_actions (action: InstanceActionInfo) with NO_CONTENT;
+    GET "/balloon" as get_balloon -> Balloon with OK;
+    PUT "/balloon" as put_balloon (balloon: Balloon) with NO_CONTENT;
+    PATCH "/balloon" as patch_balloon (balloon_patch: BalloonUpdate) with NO_CONTENT;
+    GET "/balloon/statistics" as get_balloon_statistics -> BalloonStats with OK;
+    PATCH "/balloon/statistics" as patch_balloon_statistics (balloon_stats_update: BalloonStatsUpdate) with NO_CONTENT;
+    PATCH "/balloon/hinting/start" as patch_balloon_hinting_start (balloon_start_cmd: BalloonStatsUpdate) with OK;
+    GET "/balloon/hinting/status" as get_balloon_hinting_status -> BalloonHintingStatus with OK;
+    PUT "/boot-source" as put_boot_source (boot_source: BootSource) with NO_CONTENT;
+    PUT "/cpu-config" as put_cpu_config (cpu_config: CpuConfig) with NO_CONTENT;
+    PUT "/logger" as put_logger (logger: Logger) with NO_CONTENT;
+    GET "/machine-config" as get_machine_config -> MachineConfiguration with OK;
+    PUT "/machine-config" as put_machine_config (machine_config: MachineConfiguration) with NO_CONTENT;
+    PATCH "/machine-config" as patch_machine_config (machine_config: MachineConfiguration) with NO_CONTENT;
+    PUT "/metrics" as put_metrics (metrics: Metrics) with NO_CONTENT;
+    PUT "/mmds" as put_mmds (mmds: MmdsContentsObject) with NO_CONTENT;
+    PATCH "/mmds" as patch_mmds (mmds: MmdsContentsObject) with NO_CONTENT;
+    GET "/mmds" as get_mmds -> MmdsContentsObject with OK;
+    PUT "/mmds/config" as put_mmds_config (mmds_config: MmdsConfig) with NO_CONTENT;
+    PUT "/entropy" as put_entropy (device: EntropyDevice) with NO_CONTENT;
+    PUT "/serial" as put_serial (device: SerialDevice) with NO_CONTENT;
+    PUT "/hotplug/memory" as put_hotplug_memory(config: MemoryHotplugConfig) with NO_CONTENT;
+    PATCH "/hotplug/memory" as patch_hotplug_memory (memory_hotplug_size_update: MemoryHotplugSizeUpdate) with NO_CONTENT;
+    GET "/hotplug/memory" as get_hotplug_memory -> MemoryHotplugStatus with OK;
+    PUT "/snapshot/create" as put_snapshot_create (options: SnapshotCreateParams) with NO_CONTENT;
+    PUT "/snapshot/load" as put_snapshot_load (options: SnapshotLoadParams) with NO_CONTENT;
+    GET "/version" as get_version -> FirecrackerVersion with OK;
+    PATCH "/vm" as patch_vm (vm: VmState) with NO_CONTENT;
+    GET "/vm/config" as get_vm_config -> FullVmConfiguration with OK;
+    PUT "/vsock" as put_vsock (vsock: Vsock) with NO_CONTENT;
+
+
+    // INVALID ROUTE, IMPLEMENT MANUALLY
+    // [*] PATCH "/balloon/hinting/stop" as patch_balloon_hinting_stop with OK;
+    //
+    // [*] PUT "/drives/{drive_id}" (drive: Drive) with NO_CONTENT;
+    // [*] PATCH "/drives/{drive_id}" (partial_drive: PartialDrive) with NO_CONTENT;
+    //
+    // [*] PUT "/pmem/{id}" (pmem: Pmem) with NO_CONTENT;
+    //
+    // [*] PUT "/network-interfaces/{iface_id}" (interface: NetworkInterface) with NO_CONTENT;
+    // [*] PATCH "/network-interfaces/{iface_id}" (interface: PartialNetworkInterface) with NO_CONTENT;
+);
+
+impl FirecrackerApiClient {
+    pub async fn patch_balloon_hinting_stop(&self) -> Result<(), ApiError> {
+        self.patch("/balloon/hinting/stop", Vec::new(), StatusCode::OK)
+            .await
+    }
+
+    pub async fn put_drives(&self, drive: &dto::Drive) -> Result<(), ApiError> {
+        self.put(
+            format!("/drives/{}", drive.drive_id).as_str(),
+            serde_json::to_vec(drive)?,
+            StatusCode::NO_CONTENT,
+        )
+        .await
+    }
+
+    pub async fn patch_drives(&self, partial_drive: &dto::PartialDrive) -> Result<(), ApiError> {
+        self.put(
+            format!("/drives/{}", partial_drive.drive_id).as_str(),
+            serde_json::to_vec(partial_drive)?,
+            StatusCode::NO_CONTENT,
+        )
+        .await
+    }
+
+    pub async fn put_pmem(&self, pmem: &dto::Pmem) -> Result<(), ApiError> {
+        self.put(
+            format!("/pmem/{}", pmem.id).as_str(),
+            serde_json::to_vec(pmem)?,
+            StatusCode::NO_CONTENT,
+        )
+        .await
+    }
+
+    pub async fn put_network_interface(
+        &self,
+        interface: &dto::NetworkInterface,
+    ) -> Result<(), ApiError> {
+        self.put(
+            format!("/network-interfaces/{}", interface.iface_id).as_str(),
+            serde_json::to_vec(interface)?,
+            StatusCode::NO_CONTENT,
+        )
+        .await
+    }
+
+    pub async fn patch_network_interface(
+        &self,
+        interface: &dto::PartialNetworkInterface,
+    ) -> Result<(), ApiError> {
+        self.patch(
+            format!("/network-interfaces/{}", interface.iface_id).as_str(),
+            serde_json::to_vec(interface)?,
+            StatusCode::NO_CONTENT,
+        )
+        .await
     }
 }
